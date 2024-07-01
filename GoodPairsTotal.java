32ae47957b0eac621819a8d2f351c69ec80d2972
Sophia Oshin <oshiesophz@gmail.com>
2024-07-01 16:02:15 +0530

    Number of Good Pairs

---
+++ GoodPairsTotal.java
@@ -0,0 +1,30 @@
+/*1512. Number of Good Pairs
+Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ * 
+ */
+public class GoodPairsTotal {
+    public static void main(String[] args) {
+        int [] nums = {1,2,34,3,4,5,7,23,12};  
+        numIdenticalPairs(nums);
+     } 
+    static int numIdenticalPairs(int[] nums) {
+        int count =0;
+        for(int i=0; i<=nums.length-1; i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i] == nums[j] ){
+                    System.out.print(nums[i]);
+                      System.out.println(nums[j]);
+                        count++;
+
+                }
+            }
+        }
+        return count;
+    }
+}
--- ThreeConsecutiveOdds.java
+++ ThreeConsecutiveOdds.java
@@ -21,7 +21,6 @@ Explanation: [5,7,23] are three consecutive odds.
        threeConsecutiveOdds(arr);
      } 
      static boolean threeConsecutiveOdds(int[] arr) {
-        //  boolean bool = false || false;
         for (int i=0;i<=arr.length-2;i++){
             if((arr[i]%2==1) && (arr[i+1]%2==1) && (arr[i+2]%2==1))
             {
