f871edfa58e5b5359fdb745f70997683e02baa64
Sophia Oshin <oshiesophz@gmail.com>
2024-07-02 12:20:57 +0530

    shuffle the array

--- GoodPairsTotal.java
+++ GoodPairsTotal.java
@@ -18,10 +18,7 @@ public class GoodPairsTotal {
         for(int i=0; i<=nums.length-1; i++){
             for(int j=i+1;j<=nums.length-1;j++){
                 if(nums[i] == nums[j] ){
-                    System.out.print(nums[i]);
-                      System.out.println(nums[j]);
                         count++;
-
                 }
             }
         }
